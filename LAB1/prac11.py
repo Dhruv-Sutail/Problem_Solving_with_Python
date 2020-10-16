88ac621bc1eca1439713984652ef8ddeed1d5309
Dhruv-Sutail <72209753+Dhruv-Sutail@users.noreply.github.com>
2020-10-16 19:41:52 +0530

    Added Another Solution

--- LAB1/prac11.py
+++ LAB1/prac11.py
@@ -18,3 +18,12 @@ import calendar
 td=datetime.date.today()
 dob=datetime.date(2000,12,1)
 print(str(((td-dob)//365))+"Years"+"\n"+"Calendar "+"\n"+calendar.month(dob.year,dob.month))
+
+#Another Solution
+import calendar
+import datetime
+b=(datetime.datetime.today())
+v=(datetime.datetime(1975,12,19))
+a=b-c
+print(a)
+print(calendar.month(1975,12))
